c3d6b8fee8ed849df034eee0338bc93fbfdab73b
gferreiram97 <gabriiel.fmarinho@gmail.com>
2022-08-16 16:12:08 -0300

    add pmd

--- src/main/java/com/checkstyle/resource/CheckStyleResource.java
+++ src/main/java/com/checkstyle/resource/CheckStyleResource.java
@@ -5,16 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/checkstyle")
 public class CheckStyleResource {
 
     @GetMapping
-    public ResponseEntity<String> hellow(final String name, final String lastName) {
-        final List<String> lsit = null;
-        final var variable = "Hello Word";
-        return ResponseEntity.ok(variable);
+    public ResponseEntity<String> hello() {
+        final var helloVariable = "Hello Word";
+        return ResponseEntity.ok(helloVariable);
     }
 }
